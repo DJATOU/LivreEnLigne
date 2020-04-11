72fef851ba4a742f5a324058dfdafb0be7bcf94d
tayeb <tayebxps@gmail.com>
2020-04-11 04:00:17 +0200

    Completed the backend dev feature one

---
+++ onlineBookstore/src/main/java/ecole/bookStore/onlineBookstore/repository/BookCategoryRepository.java
@@ -0,0 +1,14 @@
+package ecole.bookStore.onlineBookstore.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import ecole.bookStore.onlineBookstore.entity.BookCategory;
+
+
+@RepositoryRestResource(collectionResourceRel="bookCateogry", path="book-category")
+public interface BookCategoryRepository extends JpaRepository<BookCategory, Long>{
+
+}
---
+++ onlineBookstore/src/main/java/ecole/bookStore/onlineBookstore/repository/BookRepository.java
@@ -0,0 +1,19 @@
+package ecole.bookStore.onlineBookstore.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import ecole.bookStore.onlineBookstore.entity.Book;
+
+
+@RepositoryRestResource(collectionResourceRel = "bookcategory",path = "book-category")
+public interface BookRepository extends JpaRepository<Book, Long>{
+	
+	@RestResource(path = "categoryid")
+	Page<Book> findByCategoryId(@Param("id") Long id, Pageable pageable);
+}
